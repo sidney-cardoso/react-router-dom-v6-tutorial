efcd660aeb0e2efd7ccc41acc0d1f369f1d1e7e7
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-10-16 23:45:52 -0300

    pagina 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './styles/global.scss'
 
 import { Home } from './components/Home'
 import { About } from './components/About'
 import { Menu } from './components/Menu'
 import { Post } from './components/Post'
-
-import './styles/global.scss'
 import { Redirect } from './components/Redirect'
+import { NotFound } from './components/NotFound'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
         <Route path="/redirect" element={<Redirect/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
   
